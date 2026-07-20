15b3b9d518bf6e22fa81f792fb76663e20d20986
Christian Lauinger <christian.lauinger@gmail.com>
2026-07-20 17:31:17 +0200

    Adapt Linux process memory lookup to procfs 0.18

--- src-tauri/src/data_harvester/processes/linux.rs
+++ src-tauri/src/data_harvester/processes/linux.rs
@@ -5,6 +5,7 @@ use std::io::{BufRead, BufReader};
 
 use fxhash::{FxHashMap, FxHashSet};
 use procfs::process::{Process, Stat};
+use procfs::WithCurrentSystemInfo;
 use sysinfo::ProcessStatus;
 
 use super::{ProcessHarvest, UserTable};
@@ -171,7 +172,7 @@ fn read_proc(
         use_current_cpu_total,
     );
     let parent_pid = Some(stat.ppid);
-    let mem_usage_bytes = stat.rss_bytes()?;
+    let mem_usage_bytes = stat.rss_bytes().get();
     let mem_usage_kb = mem_usage_bytes / 1024;
     let mem_usage_percent = mem_usage_kb as f64 / mem_total_kb as f64 * 100.0;
 
