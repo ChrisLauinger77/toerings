2a4f9adacfdeadcc2e5021e56e39b426f48a5db9
Christian Lauinger <christian.lauinger@gmail.com>
2026-09-06 09:58:12 +0200

    refactor: remove inactive native diagnostics scaffolding (#81)

--- src-tauri/src/utils/error.rs
+++ src-tauri/src/utils/error.rs
@@ -17,27 +17,15 @@ pub enum ToeError {
     /// An error when the heim library encounters a problem.
     #[error("Error caused by Heim, {0}")]
     InvalidHeim(String),
-    /// An error when the Crossterm library encounters a problem.
-    #[error("Error caused by Crossterm, {0}")]
-    CrosstermError(String),
     /// An error to represent generic errors.
     #[error("Error, {0}")]
     GenericError(String),
-    /// An error to represent errors with fern.
-    #[error("Fern error, {0}")]
-    FernError(String),
     /// An error to represent errors with the config.
     #[error("Configuration file error, {0}")]
     ConfigError(String),
-    /// An error to represent errors with converting between data types.
-    #[error("Conversion error, {0}")]
-    ConversionError(String),
     /// An error to represent errors with querying.
     #[error("Query error, {0}")]
     QueryError(Cow<'static, str>),
-    /// An error that just signifies something minor went wrong; no message.
-    #[error("Minor error.")]
-    MinorError,
     /// An error to represent errors with procfs
     #[cfg(target_os = "linux")]
     #[error("Procfs error, {0}")]
@@ -66,25 +54,6 @@ impl From<std::num::ParseIntError> for ToeError {
 impl From<std::string::String> for ToeError {
     fn from(err: std::string::String) -> Self {
         ToeError::GenericError(err)
-    }
-}
-
-#[cfg(feature = "fern")]
-impl From<fern::InitError> for ToeError {
-    fn from(err: fern::InitError) -> Self {
-        ToeError::FernError(err.to_string())
-    }
-}
-
-impl From<std::str::Utf8Error> for ToeError {
-    fn from(err: std::str::Utf8Error) -> Self {
-        ToeError::ConversionError(err.to_string())
-    }
-}
-
-impl From<std::string::FromUtf8Error> for ToeError {
-    fn from(err: std::string::FromUtf8Error) -> Self {
-        ToeError::ConversionError(err.to_string())
     }
 }
 
